544957b8581705d5036ec8dc95a60aec5228aea4
Jonathan Spalink <spalink@gmail.com>
2020-11-06 10:35:31 -0600

    added an ad hoc data field to VObject to support additions like Sentiment

--- src/vutils/output/vobject.py
+++ src/vutils/output/vobject.py
@@ -3,12 +3,13 @@ from .vobject_category import VObjectCategory
 
 class VObject(object):
     
-    def __init__(self, type, text=None, label=None, sentence=None, *args, **kwargs):
+    def __init__(self, type, text=None, label=None, sentence=None, data=None, *args, **kwargs):
         self.type = type
         self.text = text
         self.label = label
         self.sentence = sentence
         self.object_category = VObjectCategory()
+        self.data = data or dict()
     
     def as_dict(self):
         r = dict(type=self.type)
@@ -20,5 +21,9 @@ class VObject(object):
             r['sentence'] = self.sentence
         if not self.object_category.is_empty:
             r['objectCategory'] = self.object_category.as_dict()
+        for k, v in self.data.items():
+            if hasattr(v, 'as_dict'):
+                v = v.as_dict()
+            r[k] = v
         return r
 
